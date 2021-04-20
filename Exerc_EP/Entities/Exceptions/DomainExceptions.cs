98584969e023a16881a00890b2b271a9ebfd66f7
Fabrício Dias <fabricio.dias102@gmail.com>
2021-04-20 15:45:00 -0300

    Exercicio sobre  exceções personalizadas

---
+++ Exerc_EP/Entities/Exceptions/DomainExceptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc_EP.Entities.Exceptions
+{
+    class DomainExceptions : ApplicationException
+    {
+        public DomainExceptions(string message) : base(message)
+        {
+
+        }
+    }
+}
